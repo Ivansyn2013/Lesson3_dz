c3005dc7cd06e1db52f3c874d90b4f47252f4803
ivan <ivansyn2013@yandex.ru>
2021-11-18 19:21:42 +0300

    не успел сделать задание со звездочкой

--- DZ_2.py
+++ DZ_2.py
@@ -3,7 +3,7 @@
 # принимающую в качестве аргументов строки в формате «Имя Фамилия» и возвращающую словарь, в котором ключи — первые буквы
 # фамилий, а значения — словари, реализованные по схеме предыдущего задания и содержащие записи,
 # в которых фамилия начинается с соответствующей буквы.
-def first_sign (my_element):
+def first_sign(my_element):
     '''
     Return first sign of element
     :param my_element:
@@ -14,9 +14,8 @@ def first_sign (my_element):
     return work_f[0]
 
 
-
-def theaurus_adv (my_name_str):
-    #подготовка данных
+def theaurus_adv(my_name_str):
+    # подготовка данных
     my_name_dict = {}
     my_name_dict.clear
     fs_list = []
@@ -27,19 +26,19 @@ def theaurus_adv (my_name_str):
     filter_names = []
     all_list = my_name_str.split(' ')
     i = 0
-    #получаем имена и фамилии
+    # получаем имена и фамилии
     while i < len(all_list):
         if i % 2 == 0:
             name_list.append(all_list[i]);
         else:
             surname_list.append(all_list[i]);
-        i +=1
-    #создание списка первых букв имен
-    fs_list = list(map (first_sign, name_list))
+        i += 1
+    # создание списка первых букв имен
+    fs_list = list(map(first_sign, name_list))
     name_str = ' '.join(all_list)
 
     print(fs_list)
-    #сортировка и фильтрация
+    # сортировка и фильтрация
     for i in range(len(name_list)):
         temp_name_list = []
         for ii in range(len(name_list)):
@@ -58,8 +57,6 @@ def theaurus_adv (my_name_str):
 
 input_names = str(input('Введите имя и фамилию:'))
 
-
-
-#print(input_names)
+# print(input_names)
 
 theaurus_adv(input_names)
